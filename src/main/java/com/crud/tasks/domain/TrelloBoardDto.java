ceb360856a341c47bc52d4f66e03d0ac55054233
“agatarajewska <agatarajewska@gmail.com>
2018-05-08 14:50:06 +0200

    17.4

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+
+}
